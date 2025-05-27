b7e90801005b1598e0505eaee5a3327ef776e563
ved9797 <vedanshivc@outlook.com>
2025-05-27 13:30:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ def main():
                              2 if x=='Wed' else 
                              3 if x=='Thus' else
                              4 if x=='Fri' else
-                             5 if x=='Sat' else
-                             6 if x=='Sun')
+                             5 if x=='Sat' else 6 )
     arr_w = weekd_lambda(st.selectbox('What is the weekday of arrival',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
     dep_w = weekd_lambda(st.selectbox('What is the weekday of departure',['Mon','Tue','Wed','Thus','Fri','Sat','Sun']))
     weekn = st.text_input('Enter the no. of week nights in stay')
